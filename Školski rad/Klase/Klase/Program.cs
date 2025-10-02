3c1c72b058148d19eedcff6e4780dc5f677af1b5
milanche9991 <milan.stojanovic@ets.edu.rs>
2025-10-02 09:42:29 +0200

    Klase

---
+++ Školski rad/Klase/Klase/Program.cs
@@ -0,0 +1,20 @@
+﻿internal class Program
+{
+    class Ucenik {
+        public string ime;
+        public string prezime;
+
+        public void PrikaziIme() {
+            Console.WriteLine("Moje ime je: " + ime + ", a prezime " + prezime);
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+
+        Ucenik ucenik = new Ucenik();
+        ucenik.ime = "Milan";
+        ucenik.prezime = "Stojanovic";
+        ucenik.PrikaziIme();
+    }
+}
